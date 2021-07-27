c1351994c35e241207f9003e0eb0d40a997653d6
Dylan Wolfe <wolfed9902@gmail.com>
2021-07-27 16:14:28 -0700

    Fix static routing

--- seeker/app.py
+++ seeker/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+app.static_folder = 'static'
 
 @app.route("/")
-def hello_world():
+def homepage():
     return render_template('home.html')
 
 if __name__ == '__main__':
