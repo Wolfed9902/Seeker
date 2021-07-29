8a0d26ba58dd21a515c392cc52157de33e3b59e8
Dylan Wolfe <wolfed9902@gmail.com>
2021-07-29 14:51:52 -0700

    Create route for listings.html

--- seeker/app.py
+++ seeker/app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 app.static_folder = 'static'
 
 @app.route("/")
+@app.route("/home")
 def homepage():
     return render_template('home.html')
+
+@app.route("/listings")
+def listingspage():
+    return render_template('listings.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
