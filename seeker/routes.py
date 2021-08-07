ff14fbe4662bcd59ef983575dc263489fd0e7297
Dylan Wolfe <wolfed9902@gmail.com>
2021-08-07 14:58:24 -0700

    Add routes.py

---
+++ seeker/routes.py
@@ -0,0 +1,20 @@
+
+from flask import render_template, url_for, redirect, request
+from seeker import app
+
+@app.route("/")
+@app.route("/home")
+def homepage():
+    return render_template('home.html')
+
+@app.route("/signin")
+def signin():
+    return render_template('signin.html')
+
+@app.route("/listings")
+def listingspage():
+    return render_template('listings.html')
+
+@app.route("/history")
+def history():
+    return render_template('history.html')
